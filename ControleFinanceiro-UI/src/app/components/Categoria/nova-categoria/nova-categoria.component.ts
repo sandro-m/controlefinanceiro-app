654e0268a1e4892a317ecc49d65af9ad0ead1cda
sandro-m <sandrohmorita@gmail.com>
2021-06-07 15:42:42 -0300

    Criando o primeiro formulário com Reactive Forms

---
+++ ControleFinanceiro-UI/src/app/components/Categoria/nova-categoria/nova-categoria.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Tipo } from 'src/app/models/Tipo';
+import { TiposService } from 'src/app/services/tipos.service';
+import { FormGroup, FormControl, FormControlDirective } from '@angular/forms';
+
+@Component({
+  selector: 'app-nova-categoria',
+  templateUrl: './nova-categoria.component.html',
+  styleUrls: ['./nova-categoria.component.css']
+})
+export class NovaCategoriaComponent implements OnInit {
+
+  formulario: any;
+  tipos : Tipo[];
+
+  constructor(private tiposService : TiposService) { }
+
+  ngOnInit(): void {
+    this.tiposService.PegarTodos().subscribe(resultado => {
+      this.tipos = resultado;
+    });
+
+    this.formulario = new FormGroup({
+      nome: new FormControl(null),
+      icone: new FormControl(null),
+      tipoId: new FormControl(null),
+    });
+  }
+
+  get propriedade () {
+    return this.formulario.controls;
+  }
+}
